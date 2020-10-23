9b4dd6f05e95fdb2bc755fdb5764c22197267cb9
Xin Chen <chenx23333@gmail.com>
2020-10-23 13:55:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,31 @@ app = Flask(__name__)
 # Params can be combined for further filtering
 @app.route('/search', methods=['GET'])
 def search():
-    abort(500, 'Unimplemented')
+	inspector = Inspector()
+	query = request.args.get('q')
+	cusin = request.args.get('cusine')
+	zipcode = request.args.get('zipcode')
+	limit = request.args.get('limit',5)	
+
+	result = []
+
+	if query:
+		for inspection in inspector.inspections:
+			if query.lower() in inspection.restaurant_name.lower():
+				result.append(inspection)
+	if cusine:
+		inspections = results or inspector.inspections
+		for inspection in inspector.inspections:
+			if cusine.lower() in inspection.cusine.lower():
+				result.append(inspection)
+	if zipcode:
+		inspections = results or inspector.inspections
+		for inspection in inspector.inspections:
+			if zipcode.lower() in inspection.zipcode.lower():
+				result.append(inspection)
+	return jsonify({"data": [result.to_json() for result in results[:limit]]})
+
+    
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
